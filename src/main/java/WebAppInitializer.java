afb4c218039ec9b27d5650805f9f00585ccea8ca
Erdoğan Mücahit Alımcı <mcht.almc@gmail.com>
2024-02-14 02:51:24 +0300

    MVCProject

---
+++ src/main/java/WebAppInitializer.java
@@ -0,0 +1,22 @@
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+
+// web.xml yerine kullanilir
+//Servlet'in baslaa yeridir.
+//Configuration Class'larinin yerlerini gosterme.
+public class WebAppInitializer  extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[0];
+    }
+}
